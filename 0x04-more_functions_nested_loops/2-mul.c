512e4241c7c39765c36ecffdc93eecbba3735cee
Danziiieee <sciarappadannia@gmail.com>
2023-09-15 05:12:00 -0700

    task 2 complete

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - checks for a digit (0 through 9).
+ * @a: a - Variable
+ * @b: b - Variable
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	Return(mul);
+}
